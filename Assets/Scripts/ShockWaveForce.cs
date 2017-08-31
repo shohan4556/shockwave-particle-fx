1a846f6c6af577a63d96d5d1139913fe1bb37872
shohan4556 <shohan4556@gmail.com>
2017-08-31 17:16:13 +0600

    project

---
+++ Assets/Scripts/ShockWaveForce.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockWaveForce : MonoBehaviour {
+
+	public float radius = 10f;
+	public float force = 5f;
+	public ParticleSystem postShockFX;
+	Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void ShockWave()
+	{
+		Vector3 pos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+		foreach(Collider col in colliders){
+			rigidbody = col.GetComponent<Rigidbody>();
+			if(rigidbody!=null){
+				rigidbody.AddExplosionForce(force, pos, radius);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(postShockFX.isPlaying){
+			ShockWave();
+		}	
+	}
+
+}
